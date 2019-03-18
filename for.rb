071f1e71436a4f7941175018a33ca265246abe8c
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-18 13:47:45 +0000

    Automatically backed up by Learn

--- for.rb
+++ for.rb
@@ -1,10 +1,10 @@
 
 
-def using_for
-	checklist = 1..10
-  puts "Wingardium Leviosa"
-  end
-end
+# def using_for
+# 	checklist = 1..10
+#   puts "Wingardium Leviosa"
+#   end
+# end
 
 
 checklist = 1..10
